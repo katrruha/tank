66becade0bb9aa37944a732f2768885cd3291632
927-22 <katanastasia1@gmail.com>
2025-01-25 18:02:49 +0300

    3preza

--- app/src/main/java/com/example/battletanks/MainActivity.kt
+++ app/src/main/java/com/example/battletanks/MainActivity.kt
@@ -9,6 +9,8 @@ import android.view.KeyEvent.KEYCODE_DPAD_DOWN
 import android.view.KeyEvent.KEYCODE_DPAD_UP
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.FrameLayout
 import com.example.battletanks.Direction.DOWN
 import com.example.battletanks.Direction.LEFT
@@ -18,23 +20,35 @@ import com.example.battletanks.databinding.ActivityMainBinding
 
 const val CELL_SIZE = 50
 
-lateint var binding: ActivityMainBinding
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val gridDrawer by lazer{
+    private var editMode = false
+    private val gridDrawer by lazy{
         GridDrawer(this)
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.title = "Menu"
     }
 
-    override fun onCreateoptionMenu(menu: Menu?): Boolean {
+    private fun switchEditMode() {
+        if (editMode) {
+            gridDrawer.removeGrid()
+            binding.materialsContainer.visibility = INVISIBLE
+        } else {
+            gridDrawer.drawGrid()
+            binding.materialsContainer.visibility = VISIBLE
+        }
+        editMode = !editMode
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.settings, menu)
         return true
     }
@@ -42,7 +56,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected (item: Menu?): Boolean{
         return when (item.itemId){
             R.id.menu_settings -> {
-                gridDrawer.drawGrid()
+                switchEditMode()
                 return true
             }
             else -> super.onOptionsItemSelected(item)
@@ -50,12 +64,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-when (keyCode){
-    KEYCODE_DPAD_UP -> move(UP)
-    KEYCODE_DPAD_DOWN -> move(DOWN)
-    KEYCODE_DPAD_LEFT -> move(LEFT)
-    KEYCODE_DPAD_RIGHT -> move(RIGHT)
-}
+        when (keyCode){
+            KEYCODE_DPAD_UP -> move(UP)
+            KEYCODE_DPAD_DOWN -> move(DOWN)
+            KEYCODE_DPAD_LEFT -> move(LEFT)
+            KEYCODE_DPAD_RIGHT -> move(RIGHT)
+        }
         return super.onKeyDown(keyCode, event)
     }
     private fun move(direction: Direction){
@@ -63,27 +77,27 @@ when (keyCode){
             UP->{
                 binding.myTank.rotation = 0f
                 if (binding.myTank.marginTop > 0) {
-                (binding.myTank.lauouParams as FrameLayout.LayoutParams).topMaqin += -CELL_SIZE
-            }
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMaqin += -CELL_SIZE
+                }
             }
             DOWN -> {
                 binding.myTank.rotation = 180f
                 if (binding.myTank.marginTop + binding.Mytank.heigt < binding.container.height / CELL_SIZE  * CELL_SIZE ){
-                (binding.myTank.lauouParams as FrameLayout.LayoutParams).topMaqin += CELL_SIZE
-            }
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMaqin += CELL_SIZE
+                }
             }
 
             LEFT -> {
                 binding.myTank.rotation = 270f
                 if (binding.myTank.marginTop > 0) {
-                (binding.myTank.lauouParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
-            }
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
+                }
             }
             RIGHT -> {
                 binding.myTank.rotation = 90f
                 if (binding.myTank.marginTop + binding.Mytank.heigt < binding.container.height / CELL_SIZE  * CELL_SIZE ){
-                (binding.myTank.lauouParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
-            }
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
+                }
             }
         }
         binding.container.removeView(binding.myTank)
